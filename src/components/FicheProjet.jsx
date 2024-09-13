5f9564aa9ff918634cf2190dc209ef6b87093b55
Halim Sahin <halimsarazin@gmail.com>
2024-09-13 10:25:50 +0200

    ajout icone/repo projet

--- src/components/FicheProjet.jsx
+++ src/components/FicheProjet.jsx
@@ -1,6 +1,9 @@
 import { useParams, Navigate } from "react-router-dom";
 import DataProjet from "../annexes/projets.json";
 import Carrousel from "./Carrousel";
+import GithubIcone from "../images/GithubIcone";
+import HtmlCssIcone from "../images/HtmlCssIcone";
+import ReactIcone from "../images/ReactIcone";
 const FicheProjet = () => {
   const { id } = useParams();
 
@@ -15,6 +18,15 @@ const FicheProjet = () => {
           <div className="fiche__content">
             <h1>{ficheProjet.title}</h1>
             <p>{ficheProjet.description}</p>
+          </div>
+          <div className="fiche__aside">
+            <a href={ficheProjet.lienrepo} aria-label="Github" target="_blank">
+              <GithubIcone />
+            </a>
+            <div className="fiche__technique">
+              <HtmlCssIcone />
+              <ReactIcone />
+            </div>
           </div>
           <Carrousel slides={ficheProjet.pictures} />
         </div>
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,20 +1,21 @@
 import GithubIcone from "../images/GithubIcone";
 import LinkedinIcone from "../images/LinkedinIcone";
-import TwitterIcone from "../images/TwitterIcone";
 const Social = () => {
   return (
     <div className="social__container">
-      <a href="https://github.com/Halimsahin?tab=repositories" target="_blank">
+      <a
+        href="https://github.com/Halimsahin?tab=repositories"
+        aria-label="Github"
+        target="_blank"
+      >
         <GithubIcone />
       </a>
       <a
         href="https://www.linkedin.com/in/halim-sahin-6075271b6/"
+        aria-label="Linkedin"
         target="_blank"
       >
         <LinkedinIcone />
-      </a>
-      <a href="#" target="_blank">
-        <TwitterIcone />
       </a>
     </div>
   );
